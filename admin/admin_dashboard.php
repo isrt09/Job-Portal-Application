e5c59096d83cd1ebc83e06c6a48a10cee9370f28
isrt09 <mazedur@gmail.com>
2020-12-12 14:56:25 +0600

    Template Segmentation

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -1,42 +1,7 @@
 <?php 
-   session_start();
-   if(isset($_SESSION['email']) == true){
-
-   }else{
-      header('location:admin_login.php');
-   }
- ?>
-<!doctype html>
-<html lang="en">
-   <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <meta name="description" content="">
-      <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-      <meta name="generator" content="Jekyll v3.8.6">
-      <title>Admin | Dashboard </title>
-      <!-- Latest compiled and minified CSS -->
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-      <!-- jQuery library -->
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-      <!-- Popper JS -->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-      <!-- Latest compiled JavaScript -->
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>//cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js
-      <!-- Custom styles for this template -->
-      <link href="css/dashboard.css" rel="stylesheet">
-      <link rel="stylesheet" href="//cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
-   </head>
-   <body>
-      <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-         <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
-         <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-         <ul class="navbar-nav px-3">
-            <li class="nav-item text-nowrap">
-               <a class="nav-link" href="logout.php">Sign out</a>
-            </li>
-         </ul>
-      </nav>
+  include('./includes/header.php'); 
+  
+?>
       <div class="container-fluid">
          <div class="row">
             <nav class="col-md-2 d-none d-md-block bg-light sidebar">
